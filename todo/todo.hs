73f1da9451a456c7c656c291e9f7306579fbe37f
Dave Baskind <dave@picketstudio.com>
2021-08-11 09:20:06 +1000

    A better todo list app

--- todo/todo.hs
+++ todo/todo.hs
@@ -1,20 +1,45 @@
-import System.IO
+import System.Environment
 import System.Directory
+import System.IO
 import Data.List
+import Control.Exception
 
 main = do
-  contents <- readFile "todo.txt"
+  (command:argList) <- getArgs
+  dispatch command argList
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
   let todoTasks = lines contents
       numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
-                        [0..] todoTasks
-  putStrLn "These are your todos:"
+        [0..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+        [0..] todoTasks
+  putStrLn "Your todos:"
   mapM_ putStrLn numberedTasks
-  putStrLn "Which to delete?"
-  choice <- getLine
-  let number = read choice
-      newTodos = unlines $ delete (todoTasks !! number) todoTasks
-  (tempName, tempHandle) <- openTempFile "." "temp"
-  hPutStr tempHandle newTodos
-  hClose tempHandle
-  removeFile "todo.txt"
-  renameFile tempName "todo.txt"
+  let number = read numberString
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+  bracketOnError (openTempFile "." "temp")
+    (\(tempName, tempHandle) -> do
+      hClose tempHandle
+      removeFile tempName)
+    (\(tempName, tempHandle) -> do
+      hPutStr tempHandle newTodoItems
+      hClose tempHandle
+      removeFile fileName
+      renameFile tempName fileName)
