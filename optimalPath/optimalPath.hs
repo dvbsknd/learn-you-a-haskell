c42edd9bfd15c3f98685fad708f7fe98348e07b9
Dave Baskind <dave@picketstudio.com>
2021-08-19 09:40:46 +1000

    Optimal path example

---
+++ optimalPath/optimalPath.hs
@@ -0,0 +1,36 @@
+data Section = Section { getA :: Int, getB :: Int, getC :: Int }
+  deriving (Show)
+
+type RoadSystem = [Section]
+
+heathrowToLondon = [ Section 50 10 30
+                   , Section 5 90 20
+                   , Section 40 2 25
+                   , Section 10 8 0
+                   ]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+  let (bestPathA, bestPathB) = foldl roadStep ([], []) roadSystem
+  in  if sum (map snd bestPathA) <= sum (map snd bestPathB)
+         then reverse bestPathA
+         else reverse bestPathB
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let timeA = sum (map snd pathA)
+      timeB = sum (map snd pathB)
+      forwardTimeToA = timeA + a
+      forwardTimeToB = timeB + b
+      crossTimeToA   = timeB + b + c
+      crossTimeToB   = timeA + a + c
+      newPathToA     = if forwardTimeToA <= crossTimeToA
+                          then (A, a):pathA
+                          else (C, c):(B, b):pathB
+      newPathToB     = if forwardTimeToB <= crossTimeToB
+                          then (B, b):pathB
+                          else (C, c):(A, a):pathA
+  in (newPathToA, newPathToB)
