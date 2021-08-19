4dc4758442e55b6b10debf4b964f7ca7a764c70d
Dave Baskind <dave@picketstudio.com>
2021-08-19 10:05:32 +1000

    Take a road system as input

--- optimalPath/optimalPath.hs
+++ optimalPath/optimalPath.hs
@@ -3,6 +3,8 @@ data Section = Section { getA :: Int, getB :: Int, getC :: Int }
 
 type RoadSystem = [Section]
 
+-- Used as input during exercise, but not in final result which 
+-- uses IO
 heathrowToLondon = [ Section 50 10 30
                    , Section 5 90 20
                    , Section 40 2 25
@@ -34,3 +36,18 @@ roadStep (pathA, pathB) (Section a b c) =
                           then (B, b):pathB
                           else (C, c):(A, a):pathA
   in (newPathToA, newPathToB)
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _  = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a, b, c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concatMap (show . fst) path
+      pathTime = sum $ map snd path
+  putStrLn $ "Best path: " ++ pathString
+  putStrLn $ "Time taken: " ++ show pathTime
